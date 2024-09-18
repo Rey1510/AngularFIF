2a4fe534647f4aac388bcadeb066c08a3bfbde09
Reynard A. <reynardaurele1510@gmail.com>
2024-09-18 13:22:48 +0700

    -

--- src/app/service/postdata/postdata.service.ts
+++ src/app/service/postdata/postdata.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { userDataPublic } from '../../user.data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostdataService {
+
+  userData = userDataPublic
+
+  getUsers(){
+    return this.userData
+  }
+
+  postUsers(event : any) {
+    this.userData.push(event)
+  }
 
   constructor() { }
 }
