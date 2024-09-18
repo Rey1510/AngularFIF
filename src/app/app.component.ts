ab113ebe03863c3cae41b1a430df38726f2dbea6
Reynard A. <reynardaurele1510@gmail.com>
2024-09-18 16:49:26 +0700

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormComponent } from "../form/form.component";
 import { TableComponent } from '../table/table.component';
+import { PostdataService } from './service/postdata/postdata.service';
 
 @Component({
   selector: 'app-root',
@@ -20,65 +21,9 @@ export class AppComponent implements OnInit{
 
 
   addUserForm! : FormGroup;
-
-<<<<<<< Updated upstream
-  // constructor() {
-  //   this.addUserForm = new FormGroup({
-  //     name: new FormControl('',[Validators.required, Validators.minLength(3)]),
-  //     email: new FormControl('',[Validators.required, Validators.email]),
-  //     city: new FormControl('',[Validators.required]),
-  //     province: new FormControl('',[Validators.required]),
-  //     zipcode: new FormControl('',[Validators.required])
-  //   })
-  // }
-
-  // get nameForm(){
-  //   return this.addUserForm.get('name');
-  // }
-  // get emailForm(){
-  //   return this.addUserForm.get('email');
-  // }
-  // get cityForm(){
-  //   return this.addUserForm.get('city');
-  // }
-  // get zipcodeForm(){
-  //   return this.addUserForm.get('zipcode');
-  // }
-  // get provinceForm(){
-  //   return this.addUserForm.get('province');
-  // }
-
-  // @HostListener('mouseenter') onMouseEnter() {
-  //   this.backgroundColor = 'red'
-  // }
   
-  // @HostListener('mouseleave') onMouseLeave() {
-  //   this.backgroundColor = 'blue'
-  // }
-
-  ngOnInit(): void {
-    this.title = 'FIF Challenge 1';
-    this.dataUser = [{
-      name: 'Reyy',
-      email: 'reynard@gmail.com',
-      address: {
-        city: 'Jakarta',
-        province: 'Jaksel',
-        zipcode: 1231312
-      },
-      
-    },{
-      name: 'Reyy',
-      email: 'reynard@gmail.com',
-      address: {
-        city: 'Jakarta',
-        province: 'Jaksel',
-        zipcode: 1231312
-    }}]
-  }
-=======
   constructor(
-    private postDataService : PostdataService
+    private postDataService : PostdataService;
   ) {
     
   }
@@ -88,7 +33,6 @@ export class AppComponent implements OnInit{
     this.dataUser = this.postDataService.getUsers();
   }
 
->>>>>>> Stashed changes
   checkOutput(event:any){
     this.dataUser.push(event)
   }
